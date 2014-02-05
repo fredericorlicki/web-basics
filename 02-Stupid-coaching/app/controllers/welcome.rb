c925abfe49368ee2ad2faab283417d8f718ae900
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-02-05 15:37:34 +0100

    solution stupid coaching

--- 02-Stupid-coaching/app/controllers/welcome.rb
+++ 02-Stupid-coaching/app/controllers/welcome.rb
@@ -2,14 +2,16 @@ get '/' do
   @coach_message = params[:coach_message] || "what do you want kid ?"
   # Look in app/views/index.erb
   
-  puts "self : #{self}"
-  puts "self class: #{self.class}"
-  puts "self instance variables from GET '/' route handler: #{self.instance_variables}"
   erb :index
 end
 
 post '/coach' do
-  puts "self instance variable from POST '/coach' route handler: #{self.instance_variables}"
-  
-  "Implement your coach response ! params => #{params.inspect}"
+	@coach_question = params[:what]
+	if @coach_question.include? "?"
+		@coach_answer = "silly question. Go to workout, son"
+	else 
+		@coach_answer = "you suck! go to workout, son"
+	end
+
+  erb :coach
 end
